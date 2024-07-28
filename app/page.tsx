898a3e7b8e6ccfc5a7d26d4e9e90a1437061d75a
sidharthh <sidharthpandro50@gmail.com>
2024-07-28 15:35:58 +0530

    half of way done

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import About from "@/components/about";
+import { IconCloudDemo } from "@/components/cloud";
+import { HeroSection } from "@/components/heroSection";
+
+export default function Home() {
+  return (
+    <>
+    <HeroSection />
+    <About />
+    <IconCloudDemo />
+    </>
+  );
+}
